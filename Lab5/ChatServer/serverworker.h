0b73ec9fd1864182e6a32504847378cac6dcd56f
xxx901 <2513270113@qq.com>
2026-01-02 14:35:28 +0800

    fix: 移除Lab5/ChatClient中的嵌套git仓库

---
+++ Lab5/ChatServer/serverworker.h
@@ -0,0 +1,33 @@
+#ifndef SERVERWORKER_H
+#define SERVERWORKER_H
+
+#include <QObject>
+#include <QTcpServer>
+
+class ServerWorker : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ServerWorker(QObject *parent = nullptr);
+    virtual bool setSocketDescriptor(qintptr socketDescriptor);
+
+    QString userName();
+    void setUserName(QString user);
+
+signals:
+    void logMessage(const QString &mag);
+    void jsonReceived(ServerWorker *sender,const QJsonObject &docObj);
+    void disconnectedFromClient();
+
+private:
+    QTcpSocket * m_serverSocket;
+    QString m_userName;
+
+
+public slots:
+    void onReadyRead();
+    void sendMessage(const QString &text,const QString &type = "message");
+    void sendJson(const QJsonObject &json);
+};
+
+#endif // SERVERWORKER_H
